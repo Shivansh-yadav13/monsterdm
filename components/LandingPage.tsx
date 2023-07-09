c41c14e735ce4594ada9bd21edb77e6b634d2478
Shivansh Yadav <yadavshivansh@gmail.com>
2023-07-09 17:12:34 +0530

    added steps on landing page

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -46,6 +46,60 @@ export default function LandingPage() {
                     <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl italic">
                         Increase your 💵MRR
                     </p>
+                    <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-3xl font-bold italic">
+                        How does it work?
+                    </p>
+                    <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-3xl font-bold italic">
+                        <p className="text-xl font-normal">
+                            Start by adding your Instagram accounts. There is no Limit to adding instagram accounts.
+                        </p>
+                    </p>
+                    <div className="w-full">
+                        <img className="w-20 mx-auto my-20" src="/arrow-down.png" alt="down-arrow" />
+                    </div>
+                    <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-3xl font-bold italic">
+                        Instagram Profile
+                        <p className="text-xl font-normal">
+                            It will start by visiting the instagram profile of your lead
+                        </p>
+                    </p>
+                    <div className="w-full">
+                        <img className="w-20 mx-auto my-20" src="/arrow-down.png" alt="down-arrow" />
+                    </div>
+                    <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-3xl font-bold italic">
+                        Follow Lead
+                        <p className="text-xl font-normal">
+                            Depending on the options you choose, It will follow the lead.
+                            If you are already following, then It will skip.
+                        </p>
+                    </p>
+                    <div className="w-full">
+                        <img className="w-20 mx-auto my-20" src="/arrow-down.png" alt="down-arrow" />
+                    </div>
+                    <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-3xl font-bold italic">
+                        Like & Comment Posts
+                        <p className="text-xl font-normal">
+                            After hitting the follow button, our software will detect if the user has posted anything, and accordingly will start liking and commenting on the posts.
+                            The software will like the last 3 posts and comment on the last post done by the lead.
+                        </p>
+                    </p>
+                    <div className="w-full">
+                        <img className="w-20 mx-auto my-20" src="/arrow-down.png" alt="down-arrow" />
+                    </div>
+                    <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-3xl font-bold italic">
+                        Send Direct Message
+                        <p className="text-xl font-normal">
+                            At last it will send a direct message to the lead, which will be provided by you.
+                        </p>
+                    </p>
+                    <div className="my-20">
+                        <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-3xl font-bold italic">
+                            Keeping it Simple
+                        </p>
+                        <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-3xl font-bold italic">
+                            Cheapest Price in the Market 🙌
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -20,8 +20,8 @@ export default async function Navbar() {
       <div className="max-w-6xl px-6 mx-auto">
         <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
           <div className="flex items-center flex-1">
-            <Link href="/" className={s.logo} aria-label="Logo">
-              <Logo />
+            <Link href="/" className="font-extrabold text-xl" aria-label="Logo">
+              MonsterDM
             </Link>
             <nav className="hidden ml-6 space-x-2 lg:block">
               <Link href="/" className={s.link}>
